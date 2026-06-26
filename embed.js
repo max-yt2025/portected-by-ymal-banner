68f424c473d1672fd915424523d27124954fb53c
Max Crafter YT <161175370+max-yt2025@users.noreply.github.com>
2026-06-26 15:44:36 -0400

    Create embed.js for Yamal protection banner
    Added a banner for Yamal protection with styling and iframe.

---
+++ embed.js
@@ -0,0 +1,20 @@
+<!-- Add to your <head> -->
+<style>
+    .ymal-protection-banner {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 45px;
+        border: none;
+        z-index: 999999;
+    }
+    
+    /* Push your content down so banner doesn't overlap */
+    body {
+        padding-top: 45px;
+    }
+</style>
+
+<!-- Add right after <body> -->
+<iframe src="https://YOUR_USERNAME.github.io/ymalbanner/" class="ymal-protection-banner"></iframe>
